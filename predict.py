7787a6e92c4b25ba28c328c424e952b8e2983316
Pablo Ivulic <pablo@ivulic.com>
2019-02-06 22:22:05 +0100

    Loads data and prints tail of it

--- predict.py
+++ predict.py
@@ -4,12 +4,15 @@ import matplotlib
 matplotlib.use('TkAgg')
 import pathlib
 
-import pandas as pd
+import pandas
 import seaborn as sns
 
-import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
-print(tf.__version__)
+dataset_path = keras.utils.get_file("auto-mpg.data", "https://archive.ics.uci.edu/ml/machine-learning-databases/auto-mpg/auto-mpg.data")
+column_names = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight', 'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pandas.read_csv(dataset_path, names=column_names,  na_values="?", comment='\t', sep=" ", skipinitialspace=True)
 
+dataset = raw_dataset.copy()
+print(dataset.tail())

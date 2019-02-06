88780840247322b7ce038f4ec9db64064eea89d4
Pablo Ivulic <pablo@ivulic.com>
2019-02-06 22:09:07 +0100

    Installs and imports tensorflow and seaborn

---
+++ predict.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+import pathlib
+
+import pandas as pd
+import seaborn as sns
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+print(tf.__version__)
+
